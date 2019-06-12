0aa86442c326babedb69357ea1a5a25b5682c8c2
Ling <18613979073@163.com>
2019-06-12 16:53:03 +0800

    ss

--- src/test/java/com/newer/medicine1/Test.java
+++ src/test/java/com/newer/medicine1/Test.java
@@ -3,6 +3,6 @@ package com.newer.medicine1;
 public class Test {
     @org.junit.Test
     public void test(){
-        System.out.println("sssss");
+        System.out.println("sssssss");
     }
 }
