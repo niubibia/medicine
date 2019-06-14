a6ffe976047963f9cd128c3b51b040ceea809eb6
xiaojie2022 <510133805@qq.com>
2019-06-14 15:54:47 +0800

    订单日志

--- src/test/java/com/newer/medicine/ApplicationTests.java
+++ src/test/java/com/newer/medicine/ApplicationTests.java
@@ -1,11 +1,20 @@
 package com.newer.medicine;
 
 
+<<<<<<< Updated upstream
 import com.newer.medicine.domain.ErpFdproform;
 import com.newer.medicine.domain.ErpRoleModel;
 import com.newer.medicine.domain.ErpStaff;
 import com.newer.medicine.domain.Erp_purchase;
 import com.newer.medicine.mapper.Erp_purchaseMapper;
+=======
+
+import com.newer.medicine.domain.ErpLog;
+import com.newer.medicine.domain.Erp_Keids;
+import com.newer.medicine.domain.Erp_purchase;
+import com.newer.medicine.server.ErpLogServer;
+import com.newer.medicine.server.Erp_KeidsServer;
+>>>>>>> Stashed changes
 import com.newer.medicine.server.Erp_purchaseServer;
 import com.newer.medicine.service.ErpFdproformService;
 import com.newer.medicine.service.ErpRoleModelService;
@@ -25,14 +34,30 @@ import java.util.Map;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
+<<<<<<< Updated upstream
  @Autowired
     private Erp_purchaseMapper erp_purchaseMapper;
     private BCryptPasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
+=======
+    @Autowired
+    private Erp_KeidsServer erp_keidsServer;
+>>>>>>> Stashed changes
 
 @Test
     public void add(){
     System.out.println(passwordEncoder.encode("admin"));
 }
 
+<<<<<<< Updated upstream
+=======
+    @Test
+    public void contextLoads() {
+
+        List<Erp_Keids> erp_applyassetList=erp_keidsServer.findStation();
+        erp_applyassetList.forEach(s->{
+            System.out.println(s.getKinId());
+        });
+    }
+>>>>>>> Stashed changes
 
 }
