d89258b65ecc5702c59dfba373ccd78f11aa8b22
ElaineMis <1350396534@qq.com>
2019-06-14 16:30:11 +0800

    resolve confilct

--- src/test/java/com/newer/medicine/ApplicationTests.java
+++ src/test/java/com/newer/medicine/ApplicationTests.java
@@ -1,24 +1,8 @@
 package com.newer.medicine;
 
-
-<<<<<<< Updated upstream
-import com.newer.medicine.domain.ErpFdproform;
-import com.newer.medicine.domain.ErpRoleModel;
-import com.newer.medicine.domain.ErpStaff;
-import com.newer.medicine.domain.Erp_purchase;
-import com.newer.medicine.mapper.Erp_purchaseMapper;
-=======
-
-import com.newer.medicine.domain.ErpLog;
 import com.newer.medicine.domain.Erp_Keids;
-import com.newer.medicine.domain.Erp_purchase;
-import com.newer.medicine.server.ErpLogServer;
+import com.newer.medicine.mapper.Erp_purchaseMapper;
 import com.newer.medicine.server.Erp_KeidsServer;
->>>>>>> Stashed changes
-import com.newer.medicine.server.Erp_purchaseServer;
-import com.newer.medicine.service.ErpFdproformService;
-import com.newer.medicine.service.ErpRoleModelService;
-import com.newer.medicine.util.Pages;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,22 +18,21 @@ import java.util.Map;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
-<<<<<<< Updated upstream
+
  @Autowired
     private Erp_purchaseMapper erp_purchaseMapper;
     private BCryptPasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
-=======
+
     @Autowired
     private Erp_KeidsServer erp_keidsServer;
->>>>>>> Stashed changes
+
 
 @Test
     public void add(){
     System.out.println(passwordEncoder.encode("admin"));
 }
 
-<<<<<<< Updated upstream
-=======
+
     @Test
     public void contextLoads() {
 
@@ -58,6 +41,6 @@ public class ApplicationTests {
             System.out.println(s.getKinId());
         });
     }
->>>>>>> Stashed changes
+
 
 }
