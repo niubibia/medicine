78cfb5527c5c0eb8d058b1058f2a9be9a64c6233
MaToooo <34564398+MaToooo@users.noreply.github.com>
2019-06-17 10:49:44 +0800

    编辑功能

--- src/main/java/com/newer/medicine/service/ErpProindentService.java
+++ src/main/java/com/newer/medicine/service/ErpProindentService.java
@@ -1,6 +1,7 @@
 package com.newer.medicine.service;
 
 import com.newer.medicine.domain.ErpProindent;
+import com.newer.medicine.domain.ErpProindentDetail;
 import com.newer.medicine.mapper.ErpProindentMapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,5 +34,29 @@ public class ErpProindentService {
             String indentState
     ){
         return erpProindentMapper.selectCount(indentNumber,indentUrgency,state,indentState);
+    }
+
+    //删除生产订单
+    public int delPROINDENT(String indentId){
+        return erpProindentMapper.delPROINDENT(indentId);
+    }
+
+    //按主键查找生产订单详情
+    public List<ErpProindentDetail> selectById(String indentId){
+        return erpProindentMapper.selectById(indentId);
+    }
+
+    //修改生产订单的数量
+    public int  updateProindentsByIddd(int entdeNum,
+                                       double entdePrice,
+                                       String entdeId  ){
+        return erpProindentMapper.updateProindentsByIddd(entdeNum,entdePrice,entdeId);
+    }
+
+    //修改订单中的总数量和总价格
+    public int updateProindentsPriceeee(int indentCount,
+                                        double indentMoney,
+                                        String indentId){
+        return erpProindentMapper.updateProindentsPriceeee(indentCount,indentMoney,indentId);
     }
 }
